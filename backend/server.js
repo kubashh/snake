54830301345af6d6fb0e5d8bd0253be818875688
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-15 18:50:19 +0000

    0.1.14

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from "express"
+import cors from "cors"
 import { createServer } from "http"
 import { Server } from "socket.io"
 
 const port = 4000
 
-const app = express()
+const app = express(cors())
 
 const server = createServer(app)
 
-const io = new Server(server)
+const io = new Server(server,{
+  cors: {
+    orygin: "https://verbose-succotash-69gq79965wq6fwgx.github.dev:4000",
+    methods: ["GET", "POST"]
+  }
+})
 
 io.on(`connection`, (socket) => {
   console.log(`User connected ${socket.id}`)
