486d747fcc08be92642ce4711c63d3527a4b4e68
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-15 20:36:56 +0000

    0.2.29

--- frontend/src/routes/socket.js
+++ frontend/src/routes/socket.js
@@ -5,9 +5,7 @@ const address = "https://verbose-succotash-69gq79965wq6fwgx-4000.app.github.dev"
 export const setUpSocket = () => {
   global.data.socket = connect(address)
 
-  global.data.socket.on(`conection`, () => {
-    alert(`conected!`)
+  global.data.socket.on(`connect`, () => {
+    alert(`connected!`)
   })
-
-  alert(global.data.socket.conection)
 }
