066760e6c6526dc034fb677f25cf03f935952501
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-15 17:18:21 +0000

    0.0.2

--- frontend/src/routes/Menu.js
+++ frontend/src/routes/Menu.js
@@ -1,12 +1,10 @@
-import { createPortal } from "react"
-
 export const Menu = () => {
   let connected = false
 
   return (
     <div>
       <div>
-        {connected ? <div>connected</div> : <div style={{color:0xff000}}>not connected</div>}
+        {connected ? <div>connected</div> : <div style={{color: "red"}}>not connected</div>}
       </div>
       <h1>Menu</h1>
     </div>
