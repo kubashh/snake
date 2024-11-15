610484dbc36a311b7c475ad4dbd804e8b471d196
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-15 18:36:58 +0000

    0.1.12

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from "express"
+import { createServer } from "http"
+import { Server } from "socket.io"
 
 const port = 4000
 
 const app = express()
 
-app.listen(port, () => {
+const server = createServer(app)
+
+const io = new Server(server)
+
+server.listen(port, () => {
   console.log(`Snake listening on port ${port}...`)
 })
