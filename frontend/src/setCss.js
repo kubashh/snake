e6d4635c3f63f6cd4795df16c81a37089ce043f4
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-15 21:28:06 +0000

    0.2.40

--- frontend/src/setCss.js
+++ frontend/src/setCss.js
@@ -1,9 +1,7 @@
 import "./index.css"
 
 // body
-document.body.style = {
-  margin: 0,
-  fontFamily: "sans-serif",
-  backgroundColor: "black",
-  color: "white"
-}
+document.body.style.margin = 0
+document.body.style.fontFamily = "sans-serif"
+document.body.style.backgroundColor = "black"
+document.body.style.color = "white"
