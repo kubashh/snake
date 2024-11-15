af9786fed44f3b3af83e636d441bc62b37f1c6ef
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-15 21:34:45 +0000

    0.2.43

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./App"
 
+alert(`Odpalono index`)
+
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
