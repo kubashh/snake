c82d126074d618906004809e925af93de21c7941
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-15 16:22:18 +0000

    Add gitacp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+    <h1>
+      Snake
+    </h1>
+  )
 }
-
-export default App;
---
+++ gitacp.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Add gitacp"
+git push
