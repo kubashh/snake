265c4d9e122aeb734c22a103fd8c71119d2cb4ef
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-16 09:23:01 +0000

    0.4.4

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,9 @@
-import express from "express"
-import cors from "cors"
-import { createServer } from "http"
-import { Server } from "socket.io"
+import { server } from "./src/app.js"
+import { io } from "./src/io.js"
 
 const port = 4000
 
-const corsOptions = {
-  origin: "https://kubashh.github.io/snake"
-}
-
-const app = express(cors(corsOptions))
-
-const server = createServer(app)
-
-const io = new Server(server,{
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-})
-
-io.on(`connection`, (socket) => {
-  console.log(`User connected ${socket.id}`)
-})
+io(server)
 
 server.listen(port, () => {
   console.log(`Snake listening on port ${port}...`)
---
+++ backend/src/io.js
@@ -0,0 +1,14 @@
+import { Server } from "socket.io"
+
+export const io = (server) => {
+  const io = new Server(server,{
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  })
+  
+  io.on(`connection`, (socket) => {
+    console.log(`User connected ${socket.id}`)
+  })
+}
