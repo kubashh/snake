f3b26693d73390bd028830921b39352356d71be9
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-18 18:42:56 +0000

    0.5.6a

--- frontend/src/routes/setUp/setCss.js
+++ frontend/src/routes/setUp/setCss.js
@@ -1,7 +1,23 @@
-import "./index.css"
+//import "./index.css"
 
 // body
 document.body.style.margin = 0
 document.body.style.fontFamily = "sans-serif"
 document.body.style.backgroundColor = "black"
 document.body.style.color = "white"
+
+// Your CSS as text
+const styles = `
+input[type="text"], input[type="button"] {
+  background-color: black;
+  color: white;
+}
+
+input:hover {
+  cursor: pointer;
+}
+`
+
+const styleSheet = document.createElement("style")
+styleSheet.textContent = styles
+document.head.appendChild(styleSheet)
