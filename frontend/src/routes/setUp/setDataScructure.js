60b165e0017933015026b626587a4c85d76a1dd8
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-21 18:15:09 +0000

    0.6.6a

---
+++ frontend/src/routes/setUp/setDataScructure.js
@@ -0,0 +1,38 @@
+const setDataSctucture = () => {
+  if(!global.data) {
+    global.data = {}
+  }
+
+  if(global.data.setUp) {
+    alert(`Code again`)
+    return
+  } else {
+    global.data.setUp = true
+  }
+
+  // Get previous nick and color
+  if(!global.data.user) {
+    const oldData = localStorage.getItem(`data`)
+
+    let user = {
+      nick: "Nick",
+      color: "#ff0000"
+    }
+
+    if(oldData) {
+      const { nick, color } = JSON.parse(oldData)
+      if(nick && color) {
+        user.nick = nick
+        user.color = color
+      }
+    }
+
+    global.data.user = user
+  }
+
+  if(!global.data.socket) {
+    global.data.socket = {}
+  }
+}
+
+setDataSctucture()
