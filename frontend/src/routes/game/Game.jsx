438273d2b2197180a61892cb4275ffb628835dd6
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-21 17:46:22 +0000

    0.6.5g

--- frontend/src/routes/game/Game.jsx
+++ frontend/src/routes/game/Game.jsx
@@ -3,16 +3,9 @@ import { useEffect, useRef } from "react"
 export const Game = () => {
   const ref = useRef()
 
-  const draw = (ctx) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = `gray`
-    ctx.fillRect(10, 10, 100, 100)
-  }
-
   useEffect(() => {
     const canvas = ref.current
     const ctx = canvas.getContext(`2d`)
-    draw(ctx)
     global.data.ctx = ctx
     console.log(global.data.ctx)
   }, [])
