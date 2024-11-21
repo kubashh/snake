e9d4e6b48048f1b37463fddbcc6307981d87f6e0
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-21 17:24:45 +0000

    0.6.4g

--- frontend/src/routes/game/Game.js
+++ frontend/src/routes/game/Game.js
@@ -1,11 +1,27 @@
 import { getCanvas } from "./getCanvas"
+import { useEffect, useRef } from "react"
 
 export const Game = () => {
   getCanvas()
 
+  const ref = useRef()
+
+  const draw = (ctx) => {
+    ctx.clearRect(0, 0, context.canvas.width, context.canvas.height)
+    ctx.fillStyle = `gray`
+    ctx.drawRect(10, 10, 100, 100)
+  }
+
+  useEffect(() => {
+    const canvas = ref.current
+    const ctx = canvas.getContext(`2d`)
+    draw(ctx)
+  }, [])
+
   return (
     <canvas
       id="gameCanvas"
+      ref={ref}
       style={{
         display: "block",
         width: "100%",
