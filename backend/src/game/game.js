f2178d8d2396d11844a418411929e28b75e8a5e4
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-21 19:03:27 +0000

    0.6.7f

--- backend/src/game/game.js
+++ backend/src/game/game.js
@@ -3,37 +3,45 @@ import { Snake } from "./Snake.js"
 
 const update = () => {
   // Update board
+  console.time(`clearBoard`)
   clearBoard()
+  console.timeEnd(`clearBoard`)
 
+  console.time(`move`)
   for(const snake of snakes) {
     snake.move()
   }
+  console.timeEnd(`move`)
 
+  console.time(`generateApple`)
   // Generating apple
   generateApple()
+  console.timeEnd(`generateApple`)
 
+  console.time(`drawApples`)
   for(let { x, y } of apples) {
-    board[x][y] = "yellow"
+    board[x][y] = "drawApples"
   }
+  console.timeEnd(`clearBoard`)
 
+  console.time(`drawSnakes`)
   for(let { body, color } of snakes) {
     for(let { x, y } of body) {
       board[x][y] = color
     }
   }
+  console.timeEnd(`drawSnakes`)
 
+  console.time(`sendBoard`)
   for(const snake of snakes) {
     snake.sendBoard()
   }
+  console.timeEnd(`sendBoard`)
 }
 
 const fps = 1000 / 1
 
-setInterval(() => {
-  console.time()
-  update()
-  console.timeEnd()
-}, fps)
+setInterval(update, fps)
 
 
 export const dataValidation = (nick, color) => {
