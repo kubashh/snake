59ddfee8b075107d51a997608fff8c9dbabe4649
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-22 21:27:49 +0000

    1.0.0c

--- frontend/src/routes/game/responses/connectAndDisconnect.js
+++ frontend/src/routes/game/responses/connectAndDisconnect.js
@@ -1,5 +1,9 @@
 export const setConnectAndDiconect = () => {
-  global.data.socket.on(`connect`, global.data.reload)
+  global.data.socket.on(`connect`, () => {
+    global.data.reload()
+  })
 
-  global.data.socket.on(`disconnect`, global.data.reload)
+  global.data.socket.on(`disconnect`, () => {
+    global.data.reload()
+  })
 }
