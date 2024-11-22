ee0a72a395e4436b439a32cf4757d68562275276
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-22 17:14:17 +0000

    0.6.9s

--- frontend/src/routes/game/drawBox.js
+++ frontend/src/routes/game/drawBox.js
@@ -1,4 +1,4 @@
 export const drawBox = (x, y, w, h, color = "white") => {
   global.data.ctx.fillStyle = color
-  global.data.ctx.fillRect(x, global.data.ctx.canvas.height - y, w, h)
+  global.data.ctx.fillRect(x, y, w, h) //global.data.ctx.canvas.height - y
 }
