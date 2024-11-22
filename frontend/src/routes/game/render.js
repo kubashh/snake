6a9251a1fa8ddfd91eb92f0f4db7989daa9d98b6
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-22 20:50:32 +0000

    0.7.1j

--- frontend/src/routes/game/render.js
+++ frontend/src/routes/game/render.js
@@ -34,8 +34,8 @@ export const render = (data) => {
     j -= head.y
     const x = i * pixelSize + middle.x
     const y = -j * pixelSize + middle.y
-    if(0 < x && x < height && 0 < y && y < width) {
+    //if(0 < x && x < height && 0 < y && y < width) {
       drawBox(x, y, pixelSize, pixelSize, color)
-    }
+    //}
   }
 }
