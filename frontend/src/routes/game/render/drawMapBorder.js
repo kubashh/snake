ca45d05d8d3a202c784e7ef37676793b8ddc6afd
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-27 16:03:51 +0000

    1.0.6j

--- frontend/src/routes/game/render/drawMapBorder.js
+++ frontend/src/routes/game/render/drawMapBorder.js
@@ -7,4 +7,5 @@ export const drawMapBorder = (middle, head) => {
   const y = Math.floor(data.ctx.canvas.height - (middle.y - head.y * pixelSize) - boardSize * pixelSize)
 
   drawBox(x, y, boardSize * pixelSize, boardSize * pixelSize, `#000`)
+  console.log(middle, head, pixelSize, boardSize, data.ctx.canvas)
 }
