acfaacbc0bb4d07c9fe90957c25aa212411f1a80
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-27 14:40:17 +0000

    1.0.6a

--- frontend/src/routes/game/render/fillBackground.js
+++ frontend/src/routes/game/render/fillBackground.js
@@ -2,6 +2,7 @@ import { data } from "../../data"
 import { drawBox } from "../drawBox"
 
 export const fillBackground = () => {
-  const { width, height, bgColor } = data
+  const { bgColor } = data
+  const { width, height } = data.ctx.canvas
   drawBox(0, 0, width, height, bgColor)
 }
