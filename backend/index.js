0c9c71c56b5c4a07bd535d5adcdb79d038325a4e
kubashh <146523311+kubashh@users.noreply.github.com>
2024-11-27 15:32:40 +0000

    1.0.6f

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-import { server } from "./src/server.js"
+import { createServer } from "http"
 import { io } from "./src/io.js"
 import { start } from "./src/game/game.js"
 
 const port = 4000
 
+const server = createServer()
+
 io(server)
 
 server.listen(port, () => {
   console.log(`Snake on port ${port}...`)
-
-  start()
 })
+
+start()
