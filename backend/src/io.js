3c3e2c37831e9d64a05182edcb2f55b930d827c0
kubashh <146523311+kubashh@users.noreply.github.com>
2025-04-24 13:38:30 +0000

    1.0.12.1

--- backend/src/io.js
+++ backend/src/io.js
@@ -7,7 +7,7 @@ const staticData = { boardSize, appleColor }
 export const io = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000/snake2",
+      origin: "*",
     },
   })
 
