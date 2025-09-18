bc006393c4f83e1b35804825e5bbac48b050033e
Jakub <jakubhanula0@gmail.com>
2025-09-18 18:54:29 +0200

    update build (5)

--- build.ts
+++ build.ts
@@ -7,7 +7,7 @@ import { relative } from "path"
 const outdir = `./dist`
 
 const config: Bun.BuildConfig = {
-  entrypoints: [`./frontend/app/index.html`],
+  entrypoints: [`./src/app/index.html`],
   outdir,
   plugins: [plugin],
   minify: true,
@@ -18,24 +18,10 @@ const config: Bun.BuildConfig = {
   },
 }
 
-// Helper function to format file sizes
-function formatFileSize(bytes: number): string {
-  const units = [`B`, `KB`, `MB`, `GB`]
-  let size = bytes
-  let unitIndex = 0
-
-  while (size >= 1024 && unitIndex < units.length - 1) {
-    size /= 1024
-    unitIndex++
-  }
-
-  return `${size.toFixed(2)} ${units[unitIndex]}`
-}
-
-console.log(`\n🚀 Starting build process...\n`)
+console.log(`Starting build process...`)
 
 if (existsSync(outdir)) {
-  console.log(`🗑️ Cleaning previous build at ${outdir}`)
+  console.log(`Cleaning previous build...`)
   await rm(outdir, { recursive: true, force: true })
 }
 
@@ -44,14 +30,82 @@ const start = performance.now()
 // Build all the HTML files
 const result = await Bun.build(config)
 
+// Minify html code
+const htmlFile = Bun.file(`${outdir}/index.html`)
+let html = minifyHtml(await htmlFile.text())
+
+// Bundle css into html
+const cssArtefact = result.outputs.find((e) => e.path.endsWith(`.css`))
+if (cssArtefact?.path) {
+  const cssFile = Bun.file(cssArtefact.path)
+
+  const cssStart = html.indexOf(`<link rel="stylesheet"`)
+
+  let cssEnd = cssStart
+  for (; cssEnd < html.length; cssEnd++) {
+    if ([`/>`, `">`].includes(html.slice(cssEnd, cssEnd + 2))) {
+      cssEnd += 2
+      break
+    }
+  }
+
+  const slice = html.slice(cssStart, cssEnd)
+  const cssCode = `<style>${minifyHtml(await cssFile.text())}</style>`
+
+  html = html.replace(slice, cssCode)
+  cssFile.delete()
+  result.outputs.splice(result.outputs.indexOf(cssArtefact), 1)
+}
+
+htmlFile.write(html)
+
 // Print the results
-const end = performance.now()
-const buildTime = (end - start).toFixed(2)
+const buildTime = (performance.now() - start).toFixed(2)
 
-const outputTable = result.outputs.map((output) => ({
-  File: relative(process.cwd(), output.path),
-  Size: formatFileSize(output.size),
-}))
+const [outputSize, maxPathLength] = result.outputs.reduce(
+  (prev, e) => [prev[0] + e.size, Math.max(prev[1], relative(process.cwd(), e.path).length)],
+  [0, 0]
+)
 
-console.table(outputTable)
-console.log(`\n✅ Build completed in ${buildTime}ms\n`)
+const outputTable = result.outputs.reduce(
+  (prev, output) => prev + `  ${formatPath(output.path)}   ${formatFileSize(output.size)}\n`,
+  ``
+)
+console.log(`\nOutput:
+${outputTable}
+All size: ${formatFileSize(outputSize)}
+Done in ${buildTime}ms\n`)
+
+// Helper function to format file sizes
+function formatFileSize(size: number): string {
+  const units = [`B`, `KB`, `MB`, `GB`]
+  let unitIndex = 0
+
+  for (; size >= 1024 && unitIndex < units.length - 1; unitIndex++) {
+    size /= 1024
+  }
+
+  return `${size.toFixed(2)} ${units[unitIndex]}`
+}
+
+function minifyHtml(text: string) {
+  return text
+    .replaceAll(`\n`, ` `)
+    .replaceAll(/\s{2,}/g, ` `)
+    .replaceAll(/ > | >|> /g, `>`)
+    .replaceAll(/ < | <|< /g, `<`)
+    .replaceAll(/ ; | ;|; /g, `;`)
+    .replaceAll(/ { | {|{ /g, `{`)
+    .replaceAll(/ } | }|} /g, `}`)
+    .replaceAll(/ " | "|" /g, `"`)
+    .replaceAll(/ , | ,|, /g, `,`)
+  // .replaceAll(/\/\*[\s\S]*?\*\//g, ``) // Remove comments
+}
+
+function formatPath(path: string) {
+  path = relative(process.cwd(), path)
+
+  while (path.length < maxPathLength) path += ` `
+
+  return path
+}
